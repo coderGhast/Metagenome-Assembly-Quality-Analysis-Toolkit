8d70cf623b466725ef84e337b51162f0025690dd
James <euesdenje@gmail.com>
2016-03-01 10:23:55 +0000

    Setting up Spring Boot
    Set up an initial Spring Boot application to try and get what I
    currently have moved over to there.

---
+++ Web Application/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by James Euesden on 26/02/2016.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
