0b08427784ef6e8580be76e38d44918ad36a6d96
James <euesdenje@gmail.com>
2016-04-06 17:50:30 +0100

    Removing old Applications
    First application (Java) and first Spring test removed. Not necessary
    since everything is within the one WebApplication now.

--- Application/src/com/metagenomequalitytoolkit/mmp/Main.java
+++
@@ -1,9 +0,0 @@
-package com.metagenomequalitytoolkit.mmp;
-
-public class Main {
-
-    public static void main(String[] args) {
-        QualityToolkit toolkit = new QualityToolkit();
-        toolkit.run();
-    }
-}
--- Application/src/com/metagenomequalitytoolkit/mmp/tests/unit/QualityToolkitTests.java
+++
@@ -1,7 +0,0 @@
-package com.metagenomequalitytoolkit.mmp.tests.unit;
-
-/**
- * Created by James Euesden on 2/10/2016.
- */
-public class QualityToolkitTests {
-}
