72b90700fedda07051e139a54a64bd6b68463e2a
James <euesdenje@gmail.com>
2016-04-20 16:47:19 +0100

    Code for clicking on Superframe window segments
    Put in the JS needed to allow a user to click the window segments in the Superframe viewer. Doesn't display the information yet, just need to write the html format for that and JS to get the relevant information and then make it appear like magic. Horay. Once the labelling of the superframe is done I may be able to move onto other things aside code for a bit.

--- Web Application/src/main/resources/static/js/superframe-chart.js
+++ Web Application/src/main/resources/static/js/superframe-chart.js
@@ -1,7 +1,7 @@
 var superframecanvas;
 var superframecontext;
 var superframeheight = 80;
-var superframewidth = 900;
+var superframewidth = 750;
 
 function drawWindowLine(startX, endX, startY, endY){
     superframecontext.fillStyle="#000000";
@@ -25,7 +25,6 @@ function paintSuperFrame(){
                         0,
                         ((orfData[i].orfStartIndex / contigLength) * superframewidth) - ((orfData[i].orfStopIndex / contigLength) * superframewidth),
                         superframeheight / 2);
-            superframecontext.lineTo
         } else {
             superframecontext.fillRect(
                         (((orfData[i].orfStartIndex + orfData[i].frameIndicator) / contigLength) * superframewidth),
@@ -59,11 +58,34 @@ function setupSuperframeChart(){
 superframecanvas = document.getElementById("superframecanvas");
 
     for(var i=0; i < canvasList.length; i++){
-        canvasList[i].addEventListener("click", onClick, false);
         superframecanvas.width = superframewidth;
         superframecanvas.height = superframeheight;
         superframecontext = superframecanvas.getContext("2d");
     }
+    superframecanvas.addEventListener("click", superFrameClicked, false);
 
     paintSuperFrame();
+}
+
+function findSuperframeWindowClicked(x){
+    for(var i = 0; i < windowdata.length; i++){
+        if(((i * windowSize) / contigLength) * superframewidth < x &&
+            (((i + 1) * windowSize) / contigLength) * superframewidth > x){
+            console.log(i + " x: " + x);
+        }
+    }
+}
+
+function superFrameClicked(event){
+        var eventTarget;
+        if (event.srcElement){
+            eventTarget = event.srcElement;
+        } else if (event.target){
+            eventTarget = event.target;
+        }
+
+        var clientBoundary = eventTarget.getBoundingClientRect();
+        var x = event.clientX - clientBoundary.left;
+
+        findSuperframeWindowClicked(x);
 }
