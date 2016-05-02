bc604226c4c2ff6ea0b056c5755b2253a32f62fc
James <euesdenje@gmail.com>
2016-05-02 17:13:27 +0100

    Still working on that executable

--- Web Application/src/main/java/toolkit/Main.java
+++ Web Application/src/main/java/toolkit/Main.java
@@ -2,6 +2,8 @@ package toolkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 
@@ -12,7 +14,13 @@ import org.springframework.context.ApplicationContext;
  * Starts running a SpringApplication web service, defined in the pom.xml file.
  */
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Main.class, args);
     }
